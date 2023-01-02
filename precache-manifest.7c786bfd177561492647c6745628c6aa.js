f5666352994734ce2dc2fe81f0e45167b95110f9
Sebastian Salonen <sebastian.salonen@integpro.se>
2023-01-02 11:31:40 +0100

    Updates

---
+++ precache-manifest.7c786bfd177561492647c6745628c6aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6e2e269c02f29bf08563ef919a497b1",
+    "url": "/starting-react/index.html"
+  },
+  {
+    "revision": "1c10c20d70bd1bff6e15",
+    "url": "/starting-react/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "0f33a12c5620689471df",
+    "url": "/starting-react/static/js/2.3a05938b.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/starting-react/static/js/2.3a05938b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c10c20d70bd1bff6e15",
+    "url": "/starting-react/static/js/main.d841df79.chunk.js"
+  },
+  {
+    "revision": "4d06625c17ccf1e0a0dd",
+    "url": "/starting-react/static/js/runtime-main.c0b261e3.js"
+  }
+]);
